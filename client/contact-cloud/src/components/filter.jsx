6531dc147922b6f895259822791e2e46c2d79c81
Pratik Tomar <pratiksingh067@gmail.com>
2024-09-15 04:50:02 +0530

    Added Filter by Name, Number, Gender and Some modifications  in naming

--- client/contact-cloud/src/components/filter.jsx
+++ client/contact-cloud/src/components/filter.jsx
@@ -5,22 +5,43 @@ const Filter = ({ data, setSearchList }) => {
   const filterData = (criteria) => {
     let sortedData = [];
     switch (criteria) {
+      case "Name Increasing":
+        sortedData = [...data].sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Name Decreasing":
+        sortedData = [...data].sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case "Number Increasing":
+        sortedData = [...data].sort((a, b) => a.number1 - b.number1);
+        break;
+      case "Number Decreasing":
+        sortedData = [...data].sort((a, b) => b.number1 - a.number1);
+        break;
+      case "male":
+        sortedData = [...data].filter((contact) => contact.gender === "Male");
+        break;
+      case "female":
+        sortedData = [...data].filter((contact) => contact.gender === "Female");
+        break;
+      case "other":
+        sortedData = [...data].filter((contact) => contact.gender === "Other");
+        break;
       case "Recently Created":
         sortedData = [...data].sort(
           (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
         );
         break;
-      case "Recently Edited":
+      case "Recently Updated":
         sortedData = [...data].sort(
           (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
         );
         break;
-      case "Lastly Created":
+      case "Oldest Created":
         sortedData = [...data].sort(
           (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
         );
         break;
-      case "Lastly Edited":
+      case "Oldest Updated":
         sortedData = [...data].sort(
           (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt)
         );
@@ -40,6 +61,90 @@ const Filter = ({ data, setSearchList }) => {
   return (
     <div className="filter-container">
       <div className="input-container">
+        <label htmlFor="nameIncreasing">
+          <input
+            type="radio"
+            id="nameIncreasing"
+            name="filter"
+            value="Name Increasing"
+            onClick={onClickHandler}
+          />
+          Name (A-Z)
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="nameDecreasing">
+          <input
+            type="radio"
+            id="nameDecreasing"
+            name="filter"
+            value="Name Decreasing"
+            onClick={onClickHandler}
+          />
+          Name (Z-A)
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="numberIncreasing">
+          <input
+            type="radio"
+            id="numberIncreasing"
+            name="filter"
+            value="Number Increasing"
+            onClick={onClickHandler}
+          />
+          Number (0-9)
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="numberDecreasing">
+          <input
+            type="radio"
+            id="numberDecreasing"
+            name="filter"
+            value="Number Decreasing"
+            onClick={onClickHandler}
+          />
+          Number (9-0)
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="male">
+          <input
+            type="radio"
+            id="male"
+            name="filter"
+            value="male"
+            onClick={onClickHandler}
+          />
+          By Gender Male
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="female">
+          <input
+            type="radio"
+            id="female"
+            name="filter"
+            value="female"
+            onClick={onClickHandler}
+          />
+          By Gender Female
+        </label>
+      </div>
+      <div className="input-container">
+        <label htmlFor="other">
+          <input
+            type="radio"
+            id="other"
+            name="filter"
+            value="other"
+            onClick={onClickHandler}
+          />
+          By Gender Other
+        </label>
+      </div>
+      <div className="input-container">
         <label htmlFor="recentlyCreated">
           <input
             type="radio"
@@ -57,34 +162,34 @@ const Filter = ({ data, setSearchList }) => {
             type="radio"
             id="recentlyEdited"
             name="filter"
-            value="Recently Edited"
+            value="Recently Updated"
             onClick={onClickHandler}
           />
-          Recently Edited
+          Recently Updated
         </label>
       </div>
       <div className="input-container">
-        <label htmlFor="lastCreated">
+        <label htmlFor="oldestCreated">
           <input
             type="radio"
-            id="lastCreated"
+            id="oldestCreated"
             name="filter"
-            value="Lastly Created"
+            value="Oldest Created"
             onClick={onClickHandler}
           />
-          Last Created
+          Oldest Created
         </label>
       </div>
       <div className="input-container">
-        <label htmlFor="lastEdited">
+        <label htmlFor="oldestUpdated">
           <input
             type="radio"
-            id="lastEdited"
+            id="oldestUpdated"
             name="filter"
-            value="Lastly Edited"
+            value="Oldest Updated"
             onClick={onClickHandler}
           />
-          Last Edited
+          Oldest Updated
         </label>
       </div>
     </div>
